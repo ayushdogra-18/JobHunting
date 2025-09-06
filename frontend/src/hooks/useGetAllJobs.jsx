c4cfb3614ca83e4458d6a3dba8d9009fc84b1231
ayushdogra-18 <147146430+ayushdogra-18@users.noreply.github.com>
2025-09-06 15:00:18 +0530

    Fix router imports and filename issues

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -8,28 +8,42 @@ const useGetAllJobs = () => {
   const dispatch = useDispatch();
     const {searchedQuery} = useSelector(store=>store.job);
     useEffect(()=>{
+          if (!user) return;
         const fetchAllJobs = async () => {
-            try {
-                const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,{withCredentials:true});
-                if(res.data.success){
-                    dispatch(setAllJobs(res.data.jobs));
-                }
-            } catch (error) {
-                console.log(error);
-                if(error.response){
-        // Server responded with a status code outside 2xx
-        console.error("Server Response:", error.response.data);
-    } else if(error.request){
-        // Request was made but no response
-        console.error("No response from server:", error.request);
-    } else {
-        // Other errors
-        console.error("Axios config error:", error.message);
+    //         try {
+    //             const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,{withCredentials:true});
+    //             if(res.data.success){
+    //                 dispatch(setAllJobs(res.data.jobs));
+    //             }
+    //         } catch (error) {
+    //             console.log(error);
+    //             if(error.response){
+    //     // Server responded with a status code outside 2xx
+    //     console.error("Server Response:", error.response.data);
+    // } else if(error.request){
+    //     // Request was made but no response
+    //     console.error("No response from server:", error.request);
+    // } else {
+    //     // Other errors
+    //     console.error("Axios config error:", error.message);
+    // }
+    //         }
+    //     }
+    //     fetchAllJobs();
+    // },[])
+     try {
+      const res = await axios.get(`${JOB_API_END_POINT}/get?keyword=${searchedQuery}`, {
+        withCredentials: true
+      });
+      if (res.data.success) dispatch(setAllJobs(res.data.jobs));
+    } catch (error) {
+      if (error.response) console.error("Server Response:", error.response.data);
+      else if (error.request) console.error("No response from server:", error.request);
+      else console.error("Axios error:", error.message);
     }
-            }
-        }
-        fetchAllJobs();
-    },[])
+  };
+  fetchAllJobs();
+}, [user, searchedQuery, dispatch]);
 }
 
 export default useGetAllJobs
