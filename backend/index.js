47dba5d8ea30f7c0e274a869265005534d0e28fc
ayushdogra-18 <147146430+ayushdogra-18@users.noreply.github.com>
2025-09-06 14:19:52 +0530

    Fix router imports and filename issues

--- backend/index.js
+++ backend/index.js
@@ -1,54 +1,57 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';  //to parse cookies
-import cors from 'cors'; // to handle cross-origin requests
-import dotenv from 'dotenv';
-import { connect } from 'mongoose';
-import connectDB from './utils/db.js';
-import userRoutes from './routes/user.route.js';
-import companyRoutes from './routes/company.route.js';
-import jobRoutes from './routes/job.routes.js';
-import applicationRoute from './routes/application.route.js';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.routes.js";
+import applicationRoute from "./routes/application.route.js";
+
 dotenv.config({});
+
 const app = express();
-//middlewares
+
+// middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true})); // to parse urlencoded data
-app.use(cookieParser()); // to parse cookies
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http://localhost:5173',
+    credentials:true
+}
+
+app.use(cors(corsOptions));
+
+
+
+
+
 
 
 //for production---------------------------->
-const corsOptions = {
-  origin: ["http://localhost:5173", "https://job-hunt-portal-18.vercel.app"],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
-
-
-//for development-------------------------->
 // const corsOptions = {
-//   origin: "http://localhost:5173",
+//   origin: ["http://localhost:5173", "https://job-hunt-portal-18.vercel.app"],
 //   credentials: true,
 // };
-//  app.use(cors(corsOptions));
-//all api's   for eg :---> http://localhost:8000/api/vi/login
-app.use("/api/v1/user",userRoutes);
-app.use("/api/v1/company",companyRoutes);
-app.use("/api/v1/job",jobRoutes);
-app.use("/api/v1/application",applicationRoute);
+
+// app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions));
 
 
 
 const PORT = process.env.PORT || 3000;
 
 
-
-
+// api's
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 
 
 app.listen(PORT,()=>{
     connectDB();
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running at port ${PORT}`);
 })
